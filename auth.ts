8aa54734c180806c892a6e2d4c9d65fff3588a37
Femi Abimbola <femiabimbola6@gmail.com>
2024-06-05 14:53:44 -0700

    adding auth-config

--- auth.ts
+++ auth.ts
@@ -1,5 +1,12 @@
 import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
+import Twitter from "next-auth/providers/twitter";
 
 export const {handlers, signIn, signOut, auth} = NextAuth({
-  providers: [],
+  pages: {
+    signIn: "/auth/login",
+    error: "/auth/error",
+  },
+  providers: [Google, Twitter],
+  session: {strategy: "jwt"},
 });

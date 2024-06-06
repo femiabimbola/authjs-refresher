54c3a88974a3e6948ed37d8d4984619f23944266
Femi Abimbola <femiabimbola6@gmail.com>
2024-06-06 12:57:04 -0700

    initialize prisma

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,5 @@
+//Middleware code always runs in an edge runtime.
+//a database adapter that isn’t explicitly “edge compatible
+// PostgreSQL queries will not work in middleware, its uses tcp
+
 export {auth as middleware} from "@/auth";

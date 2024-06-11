52bcb0435c84ec4cb0b842d911aac0a69ae1eab8
Femi Abimbola <femiabimbola6@gmail.com>
2024-06-10 23:14:40 -0700

    adding auth modal

---
+++ components/auth/AuthModal.tsx
@@ -0,0 +1,31 @@
+"use client";
+import {useCallback, useEffect, useState} from "react";
+
+interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  actionColor?: string;
+  disabled?: boolean;
+  secondaryAction?: () => void;
+  secondaryActionLabel?: string;
+}
+
+const AuthModal = ({isOpen}: ModalProps) => {
+  const [showModal, setShowModal] = useState(isOpen);
+
+  return (
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center px-6 overflow-x-hidden overflow-y-auto outline-none overlay focus:outline-none bg-black/50"
+      // onClick={handleClose}
+    >
+      <div></div>
+    </div>
+  );
+};
+
+export default AuthModal;
--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,5 +1,14 @@
+"use client";
+import {useCallback, useEffect, useState} from "react";
 const Login = () => {
-  return <h1> The login form</h1>;
+  return (
+    <div
+      className="fixed inset-0 z-50 flex items-center justify-center px-6 overflow-x-hidden overflow-y-auto outline-none overlay focus:outline-none bg-black/50"
+      // onClick={handleClose}
+    >
+      <div></div>
+    </div>
+  );
 };
 
 export default Login;

ee0c820ffa1b4d0d4d0a67423900cbadd7db0c7f
Femi Abimbola <femiabimbola6@gmail.com>
2024-06-19 14:48:18 -0700

    setting up the user registration

--- components/auth/SignUp.tsx
+++ components/auth/SignUp.tsx
@@ -1,7 +1,92 @@
 "use client";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useForm} from "react-hook-form";
+import {RegisterSchema} from "@/lib/schemas";
+import {z} from "zod";
+import {Button} from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {Input} from "@/components/ui/input";
 
 const SignUp = () => {
-  return <div> The Signup Component</div>;
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
+    defaultValues: {
+      username: "",
+      password: "",
+      email: "",
+    },
+  });
+
+  const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
+    console.log("here");
+    console.log(values);
+  };
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="space-y-8"
+      >
+        <FormField
+          control={form.control}
+          name="username"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="shadcn"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>Enter your email</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="hello@mail.com"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({field}) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="*****"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
 };
 
 export default SignUp;
--- lib/schemas/index.ts
+++ lib/schemas/index.ts
@@ -1,3 +1,9 @@
 import {z} from "zod";
 
-export const RegisterSchema = z.object({});
+export const RegisterSchema = z.object({
+  email: z
+    .string({invalid_type_error: "Must be a string"})
+    .email({message: "Valid email is required"}),
+  password: z.string().min(6, {message: "Minimun of 6 characters required"}),
+  username: z.string().min(1, {message: "name is required"}),
+});

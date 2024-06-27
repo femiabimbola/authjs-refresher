2a38fe492713c91c202c21804c0533ee74446d2d
Femi Abimbola <femiabimbola6@gmail.com>
2024-06-27 15:00:08 -0700

    adding the login page

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,7 +1,83 @@
 "use client";
 import {useCallback, useEffect, useState} from "react";
+import {zodResolver} from "@hookform/resolvers/zod";
+import {useForm} from "react-hook-form";
+import {z} from "zod";
+import CardWrapper from "../shared/CardWrapper";
+import {LoginSchema} from "@/lib/schemas";
+import {Button} from "@/components/ui/button";
+import {InputSuffix} from "../ui/input-suffix";
+import {PasswordInput} from "../ui/password-input";
+import {MailIcon} from "lucide-react";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 const Login = () => {
-  return <div> this shoud be the login form</div>;
+  const form = useForm<z.infer<typeof LoginSchema>>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      password: "",
+      email: "",
+    },
+  });
+
+  const onSubmit = () => {};
+  return (
+    <CardWrapper title="Login">
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-8"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>Enter your email</FormLabel>
+                <FormControl>
+                  <InputSuffix
+                    placeholder="hello@mail.com"
+                    suffix={<MailIcon />}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({field}) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <PasswordInput
+                    placeholder="enter password"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="w-full"
+          >
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </CardWrapper>
+  );
 };
 
 export default Login;
--- lib/schemas/index.ts
+++ lib/schemas/index.ts
@@ -7,3 +7,10 @@ export const RegisterSchema = z.object({
   password: z.string().min(6, {message: "Minimun of 6 characters required"}),
   username: z.string().min(1, {message: "name is required"}),
 });
+
+export const LoginSchema = z.object({
+  email: z
+    .string({invalid_type_error: "Must be a string"})
+    .email({message: "Valid email is required"}),
+  password: z.string().min(6, {message: "Minimun of 6 characters required"}),
+});
